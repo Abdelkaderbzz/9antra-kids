2513f5e5ec8dbda1ae2a421540155644b024bfa8
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-15 05:23:05 +0100

    refactor: update pre-push script and restore image rendering in Map component

--- src/features/Map/Map.tsx
+++ src/features/Map/Map.tsx
@@ -1,7 +1,7 @@
-// import MAP from "../../assets/Map.svg"
+import MAP from "../../assets/Map.svg"
 
 const Map = () => {
-  return <div className="container-map">{/* <img src={MAP} alt="" className="image-map" /> */}</div>
+    return <div className="container-map"> <img src={MAP} alt="" className="image-map" /> </div>
 }
 
 export default Map
