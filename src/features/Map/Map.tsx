576929bbe4f1d8975e424a0f2f360205dd8d64a3
Rayen Mabrouk <mabroukr999@gmail.com>
2024-12-15 05:54:15 +0100

    feat: add new Map component and button functionality; update styles and imports

--- src/features/Map/Map.tsx
+++ src/features/Map/Map.tsx
@@ -1,7 +1,28 @@
-import MAP from "../../assets/Map.svg"
+import { useEffect } from 'react';
+import MAP from "../../assets/Map.svg";
+import Button from "../../assets/button.svg";
+import { useNavigate } from 'react-router-dom';
 
 const Map = () => {
-    return <div className="container-map"> <img src={MAP} alt="" className="image-map" /> </div>
-}
+    const navigate = useNavigate();
+    useEffect(() => {
+        const anchor = document.getElementById('buttom');
+        if (anchor) {
+            anchor.scrollIntoView({ behavior: 'smooth' });
+        }
+    }, []);
 
-export default Map
+    return (
+        <div className="container-map">
+            <img src={MAP} alt="" className="image-map" />
+            <span className="start-img">
+                <img onClick={() => {
+                    navigate('/lessons')
+                }} src={Button} alt="" />
+            </span>
+            <a id="buttom"></a>
+        </div>
+    );
+};
+
+export default Map;
