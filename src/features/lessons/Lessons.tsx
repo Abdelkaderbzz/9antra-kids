65cd5a16b11ccf2009193729fd199f2376366583
abdelkaderbouzmita@takiacademyteam.com <abdelkaderbouzmita@takiacademyteam.com>
2024-12-15 07:25:18 +0100

    remove unused code

--- src/features/lessons/Lessons.tsx
+++ src/features/lessons/Lessons.tsx
@@ -1,7 +1,4 @@
-import { ReactComponent as HeaderLesson } from '@assets/images/lessons/header.svg'
-import { ReactComponent as DescriptionLesson } from '@assets/images/lessons/introductions.svg'
-import { ReactComponent as VideoLesson } from '@assets/images/lessons/video.svg'
-import popupsLog from '@assets/images/auth/bg_section2.svg'
+
 import Button from '@src/components/Button/Button'
 import Video from "../../assets/video.svg"
 const Lessons = () => {
